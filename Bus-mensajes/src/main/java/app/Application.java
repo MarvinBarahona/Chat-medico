2bec50352a791a569a6d594d205bb1b3a2fe677c
fran <fran@franck>
2017-12-02 18:34:21 -0600

    Pruebas con post

--- Bus-mensajes/src/main/java/app/Application.java
+++ Bus-mensajes/src/main/java/app/Application.java
@@ -3,6 +3,8 @@ package app;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
@@ -16,20 +18,24 @@ public class Application implements WebApplicationInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        
-        
+        //Primeras pruebas con POST
+        HttpConfig peticiones = new HttpConfig();
+        NameValuePair parametro = new BasicNameValuePair("nombre", "Enviado desde Bus2");
+        NameValuePair parametro2 = new BasicNameValuePair("apellido", "Enviado desde Bus");
+        System.out.println(peticiones.httpPostSimple("http://localhost:8070/consultorio_1/medico/add", parametro, parametro2));
+         //Primeras pruebas con GET
+        System.out.println(peticiones.httpGetSimple("http://localhost:8070/consultorio_1/medico/"));
     }
-    
+
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         // Create root application context
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
         applicationContext.register(WebSocketConfig.class);
-        
-        
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);   
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
         ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("dispatcher", dispatcherServlet);
-        
+
         servletRegistration.setAsyncSupported(true);
         servletRegistration.setLoadOnStartup(1);
         servletRegistration.addMapping("/");
